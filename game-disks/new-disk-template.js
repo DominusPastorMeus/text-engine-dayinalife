2c90df78fc431e24f373f6a770b8505056be7b3a
DominusPastorMeus <91485108+DominusPastorMeus@users.noreply.github.com>
2023-11-21 00:38:13 +0200

    Update new-disk-template.js

--- game-disks/new-disk-template.js
+++ game-disks/new-disk-template.js
@@ -1,21 +1,21 @@
 // This simple game disk can be used as a starting point to create a new adventure.
 // Change anything you want, add new rooms, etc.
 const newDiskTemplate = () => ({
-  roomId: 'start', // Set this to the ID of the room you want the player to start in.
+  roomId: 'dream', // Set this to the ID of the room you want the player to start in.
   rooms: [
     {
-      id: 'start', // Unique identifier for this room. Entering a room will set the disk's roomId to this.
-      name: 'The First Room', // Displayed each time the player enters the room.
-      desc: `There's a door to the NORTH, but it is overgrown with VINES. Type ITEMS to see a list of items in the room.`, // Displayed when the player first enters the room.
+      id: 'dream', // Unique identifier for this room. Entering a room will set the disk's roomId to this.
+      name: 'The Mountain', // Displayed each time the player enters the room.
+      desc: `There you are. It's dark and it's silent. Your eyes begin to adjust. There's a path to the MOUNTAIN, but your BAG is on the ground. Type ITEMS to see a list of items in the room.`, // Displayed when the player first enters the room.
       items: [
         {
-          name: 'door',
-          desc: 'It leads NORTH.', // Displayed when the player looks at the item.
-          onUse: () => println(`Type GO NORTH to try the door.`), // Called when the player uses the item.
+          name: 'path',
+          desc: 'It leads to the MOUNTAIN.', // Displayed when the player looks at the item.
+          onUse: () => println(`Type GO MOUNTAINS to try the door.`), // Called when the player uses the item.
         },
         {
-          name: ['vines', 'vine'], // The player can refer to this item by either name. The game will use the first name.
-          desc: `They grew over the DOOR, blocking it from being opened.`,
+          name: ['bag', 'bags'], // The player can refer to this item by either name. The game will use the first name.
+          desc: `You have to pick up the bag in order to climb.`,
         },
         {
           name: 'axe',
@@ -23,37 +23,38 @@ const newDiskTemplate = () => ({
           isTakeable: true, // Allows the player to take the item.
           onUse() {
             // Remove the block on the room's only exit.
-            const room = getRoom('start');
-            const exit = getExit('north', room.exits);
+            const room = getRoom('dream');
+            const exit = getExit('The Mountain', room.exits);
 
             if (exit.block) {
               delete exit.block;
-              println(`You cut through the vines, unblocking the door to the NORTH.`);
+              println(`You picked up the bag. It is extremely heavy, but at least now you can go to the MOUNTAIN.`);
 
               // Update the axe's description.
-              getItem('axe').desc = `You USED it to cut the VINES, unblocking the door.`;
+              getItem('axe').desc = `Why is it so heavy? what's in it?.`;
             } else {
-              println(`There is nothing to use the axe on.`);
+              println(`You try to open the bag, but you can't for some reason.`);
             }
           },
         }
       ],
       exits: [
         {
-          dir: 'north', // "dir" can be anything. If it's north, the player will type "go north" to get to the room called "A Forest Clearing".
+          dir: 'mountain', // "dir" can be anything. If it's north, the player will type "go north" to get to the room called "A Forest Clearing".
           id: 'clearing',
-          block: `The DOOR leading NORTH is overgrown with VINES.`, // If an exit has a block, the player will not be able to go that direction until the block is removed.
+          block: `The PATH leading to the MOUNTAIN... you'll need a BAG.`, // If an exit has a block, the player will not be able to go that direction until the block is removed.
         },
       ],
     },
     {
-      id: 'clearing',
-      name: 'A Forest Clearing',
-      desc: `It's a forest clearing. To the SOUTH is The First Room.`,
+      id: 'path',
+      name: 'the Mountain path',
+      desc: `It’s completely dark, but simultaneously brighter than day. The sky is covered by billions upon billions of stars. 
+      So many that against the backdrop of the inky black sky, that the sky itself ceases to be seen. BACK is foot of the MOUNTAIN.`,
       exits: [
         {
-          dir: 'south',
-          id: 'start',
+          dir: 'back',
+          id: 'dream',
         },
       ],
     }
